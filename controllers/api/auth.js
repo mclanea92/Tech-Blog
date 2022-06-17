f704af2232390091f162e5f5188f018ad19e6014
Adam McLane <mclanea92@gmail.com>
2022-06-17 12:19:36 -0400

    updated apis

--- controllers/api/auth.js
+++ controllers/api/auth.js
@@ -5,14 +5,14 @@ const { User } = require('../..models');
 
 router.post('/register', async (req, res) => {
     try {
-        const userData = await User.create(
+        const Data = await User.create(
             {
                 email: req.body.email,
                 password: req.body.password,
                 username: req.body.username,
             }
         );
-        if (userData) {
+        if (Data) {
             res.status(200).send('Good')
         }
         else {
@@ -28,24 +28,24 @@ router.post('/register', async (req, res) => {
 
 router.get('/login', async (req, res) => {
     try {
-        const userData = await User.findOne({ where: {email: req.body.email}});
+        const data = await User.findOne({ where: {email: req.body.email}});
 
-        if (!userData) {
+        if (!data) {
             res.status(400).json({message: 'Wrong email or password, please try again'});
             return;
         }
-        const correctPW = await userData.checkPassword(req.body.password);
+        const correctPW = await data.checkPassword(req.body.password);
 
         if (!correctPW) {
             res.status(400).json({message: 'Wrong email or password, please try again'});
             return;
         }
         req.session.save(()=> {
-            req.session.user_id = userData.id;
+            req.session.user_id = data.id;
             req.session.loggedIn = true;
         });
 
-        res.json({user: userData, message: 'You have logged in'});
+        res.json({user: data, message: 'You have logged in'});
     }
     catch (err) {
         res.status(400).json(err);
